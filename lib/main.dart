78be90711f356de901184ce2494df5bdd6a51d13
ggaier <jwenbo52@gmail.com>
2018-12-18 23:16:06 +0800

    add icons to the list

--- lib/main.dart
+++ lib/main.dart
@@ -6,33 +6,38 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: RandomWords()
-    );
+    return MaterialApp(title: 'Welcome to Flutter', home: RandomWords());
   }
 }
 
-class RandomWords extends StatefulWidget{
+class RandomWords extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => RandomWordsState()
-
+  State<StatefulWidget> createState() => RandomWordsState();
 }
 
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
+  final _saved = Set<WordPair>();
 
-  Widget _buildRow(WordPair words) => 
-       ListTile(title: Text(words.asPascalCase, style: _biggerFont));
+  Widget _buildRow(WordPair words) {
+    final bool alreadySaved = _saved.contains(words);
+    return ListTile(
+      title: Text(words.asPascalCase, style: _biggerFont),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+    );
+  }
 
-  Widget _buildSuggestions(){
+  Widget _buildSuggestions() {
     return ListView.builder(
       padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i){
-        if(i.isOdd) return Divider();
-        final index = i ~/2;
-        if(index >= _suggestions.length){
+      itemBuilder: (context, i) {
+        if (i.isOdd) return Divider();
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10));
         }
         return _buildRow(_suggestions[index]);
@@ -43,10 +48,7 @@ class RandomWordsState extends State<RandomWords> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator')
-      ),
-      body: _buildSuggestions()
-    );
+        appBar: AppBar(title: Text('Startup Name Generator')),
+        body: _buildSuggestions());
   }
 }
